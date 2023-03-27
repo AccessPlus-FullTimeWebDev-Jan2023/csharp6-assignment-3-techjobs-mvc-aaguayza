eee26ef09417cb9438694a29aa350888f8e81c73
Tony <adaguayza@gmail.com>
2023-03-27 11:37:14 -0500

    tests done

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -10,13 +10,33 @@ namespace TechJobsMVCAutograded6.Controllers;
 
 public class SearchController : Controller
 {
+    List<Job> jobs;
     // GET: /<controller>/
     public IActionResult Index()
     {
         ViewBag.columns = ListController.ColumnChoices;
+        
+
         return View();
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+    [HttpPost]
+    [Route("/search/results")]
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        if(searchTerm == "" || searchTerm == "all")
+        {
+            jobs = JobData.FindAll();
+            ViewBag.jobs = jobs;
+
+        }
+        else{
+            jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            ViewBag.jobs = jobs; 
+        }
+        return View();
+    }
+    
 }
 
